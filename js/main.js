1260d89b14850e82c23d3daadf0fb14b947d0308
bryantee <bryantee@gmail.com>
2016-09-27 21:12:52 -0700

    Wire up initial ajax request and render state to dom
    This is the real deal.

--- js/main.js
+++ js/main.js
@@ -6,18 +6,7 @@ $(document).ready(function() {
 });
 
 var state = {
-  artists: [
-    {
-      name: "Best Band!",
-      img: "http://placekitten.com/g/150/150",
-      url: "http://last.fm/best+band"
-    },
-    {
-      name: "2nd Best Band!",
-      img: "http://placekitten.com/g/151/151",
-      url: "http://last.fm/best+band2"
-    }
-  ]
+  artists: []
 };
 
 //constructor for artist object
@@ -28,13 +17,36 @@ function Artist(name, img, url) {
   this.topTrack = undefined;
 }
 
+function getRequest(tag, limit) {
+  var params = {
+    api_key: "28013ebbad44c5793cdc84377c824554",
+    method: "tag.getTopArtists",
+    format: "json",
+    tag: tag,
+    limit: limit
+  };
+  var url = "http://ws.audioscrobbler.com/2.0";
+  // start the call
+  $.getJSON(url, params, setArtistsObject);
+}
+
+function setArtistsObject(data) {
+  data.topartists.artist.forEach(function(item, index) {
+    var name = item.name;
+    var img = item.image[1]["#text"];
+    var url = item.url;
+    var obj = new Artist(name, img, url);
+    state.artists.push(obj);
+  });
+
+}
+
 function renderData (state, parentEl) {
   var htmlEl = state.artists.map(function(item) {
-      var div = "<div class='artist'>"
-      div += "<img class='artist-img' src='" + item.img + "'>";
-      div += "<h1 class='artist-name'>" + item.name + "</h1>";
-      return div;
+    var div = "<div class='artist'>";
+    div += "<img class='artist-img' src='" + item.img + "'>";
+    div += "<h1 class='artist-name'>" + item.name + "</h1>";
+    return div;
   });
   parentEl.html(htmlEl);
-
 }
