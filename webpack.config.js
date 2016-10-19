7ab04cb3cb426c21e7011a98b1825662252f2907
bryantee <bryantee@gmail.com>
2016-10-18 17:39:01 -0700

    Complete basic npm build workflow: LESS/CSS minification
    TODO: hack workaround for .js uglify while preserving google places callback for autocomplete function.

--- webpack.config.js
+++ webpack.config.js
@@ -11,28 +11,33 @@ var minify = process.argv.indexOf('--minify') != -1;
 var plugins = [];
 
 if (minify) {
-    filename.splice(filename.length - 1, 0, 'min');
-    plugins.push(new webpack.optimize.UglifyJsPlugin());
+  filename.splice(filename.length - 1, 0, 'min');
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    mangle: false,
+    compress: {
+      unused: false
+    }
+  }));
 }
 
 module.exports = {
-    entry: path.resolve(__dirname, packageData.main),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: filename.join('.'),
-    },
-    devtool: 'source-map',
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          loader: 'babel',
-          query: {
-            presets: ['es2015']
-          }
+  entry: path.resolve(__dirname, packageData.main),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: filename.join('.'),
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
         }
-      ]
-    },
-    plugins: plugins
+      }
+    ]
+  },
+  plugins: plugins
 };
